04f3ef2d62cac1ca1e902a729db819345ef25db9
Giovanni Urdaneta <giovanniurdanetatulli@gmail.com>
2023-10-16 19:36:14 -0400

    changed folder name error to utils

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express"
-import CustomError from "../error/CustomError"
+import CustomError from "../utils/CustomError"
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
     if (err instanceof CustomError) {
