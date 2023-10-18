bd314e69046027fc0ddec80e7f6ec0e11db09dab
Giovanni Urdaneta <117308178+giout@users.noreply.github.com>
2023-10-18 19:43:01 -0400

    Update error.ts
    Error log.

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from "express"
 import CustomError from "../utils/CustomError"
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.log(err)
+    
     if (err instanceof CustomError) {
         return res.status(err.statusCode).json({ msg: err.message })
     }
