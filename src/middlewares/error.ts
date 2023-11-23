91749f8359558c42998401aa6b969884a79d7493
Giovanni Urdaneta <giovanniurdanetatulli@gmail.com>
2023-11-22 23:55:44 -0400

    CustomError imports

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express"
-import CustomError from "../utils/CustomError"
+import { CustomError } from "../utils/error.utils"
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
     console.log(err)
